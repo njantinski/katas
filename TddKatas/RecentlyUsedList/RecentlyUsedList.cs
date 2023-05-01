a1f974f02b1143a72b777696206c4a5236db0113
Nikolaj Jantinski <njantinski@gmail.com>
2023-05-01 12:49:18 +0200

    Add first 2 tests for katas

---
+++ TddKatas/RecentlyUsedList/RecentlyUsedList.cs
@@ -0,0 +1,35 @@
+﻿namespace TddKatas
+{
+    public class RecentlyUsedList
+    {
+        private int _counter;
+
+        public RecentlyUsedList()
+        {
+            Items = new List<string>();
+            _counter = 0;
+        }
+
+        public int Length { get => _counter; }
+        private List<string> Items { get; set; }
+
+        public void Add(string v)
+        {
+            if (Items.Contains(v))
+            {
+                var tmp = Items.IndexOf(v);
+                Items[tmp] = Items[_counter - 1];
+                Items[_counter - 1] = v;
+                return;
+            }
+
+            Items.Add(v);
+            _counter++;
+        }
+
+        public string getByIndex(int v)
+        {
+            return Items[(_counter - 1) - v];
+        }
+    }
+}
---
+++ TddKatasTests/RecentlyUsedList/RecentlyUsedListTests.cs
@@ -0,0 +1,50 @@
+﻿using TddKatas;
+
+namespace TddKatasTests
+{
+    public class RecentlyUsedListTests
+    {
+        [Fact(DisplayName = "Firstly added item should be added on first position, lastly added item should be last")]
+        public void Test()
+        {
+            var list = new RecentlyUsedList();
+            list.Add("Test1");
+            list.Add("Test2");
+            list.Add("Test3");
+
+            var result = list.getByIndex(0);
+            var result2 = list.getByIndex(1);
+
+            Assert.Equal("Test3", result);
+            Assert.Equal("Test2", result2);
+        }
+
+        [Fact(DisplayName = "Duplicates are moved rather than duplicated")]
+        public void Test2()
+        {
+            var list = new RecentlyUsedList();
+            list.Add("Test1");
+            list.Add("Test2");
+            list.Add("Test3");
+            list.Add("Test4");
+            list.Add("Test1");
+            list.Add("Test3");
+            list.Add("Test4");
+            list.Add("Test2");
+            list.Add("Test1");
+
+
+            var result = list.getByIndex(0);
+            var result2 = list.getByIndex(1);
+            var result3 = list.getByIndex(2);
+            var result4 = list.getByIndex(3);
+
+            Assert.Equal("Test1", result);
+            Assert.Equal("Test2", result2);
+            Assert.Equal("Test4", result3);
+            Assert.Equal("Test3", result4);
+            Assert.Equal(4, list.Length);
+        }
+
+    }
+}

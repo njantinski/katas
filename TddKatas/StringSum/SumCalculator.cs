cd4958f49a6804fe179b9a020895874872c2c8cd
Nikolaj Jantinski <njantinski@gmail.com>
2023-05-01 14:18:12 +0200

    Add string sum kata

---
+++ TddKatas/StringSum/SumCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TddKatas
+{
+    public class SumCalculator
+    {
+        public string Sum(string v1, string v2)
+        {
+            var number1 = int.Parse(Regex.Replace(v1, "[^0-9]", ""));
+            var number2 = int.Parse(Regex.Replace(v2, "[^0-9]", ""));
+            return (number1 + number2).ToString();
+        }
+
+
+    }
+}
---
+++ TddKatasTests/StringSum/StringSumTests.cs
@@ -0,0 +1,25 @@
+﻿using TddKatas;
+
+namespace TddKatasTests
+{
+    public class StringSumTests
+    {
+        [Fact]
+        public void ShouldSum2Numbers() 
+        {
+            var sumCalculator = new SumCalculator();
+            var result = sumCalculator.Sum("1","5");
+
+            Assert.Equal("6", result);
+        }
+
+        [Fact]
+        public void ShouldRemoveAllNonNumberCharacters() 
+        {
+            var sumCalculator = new SumCalculator();
+            var result = sumCalculator.Sum("121fdaffdasfasddfas", "1dfasfdasfdasfadsfafdasfdasfasd/'/'/'/'/';;';'p[");
+
+            Assert.Equal("122", result);
+        }
+    }
+}
